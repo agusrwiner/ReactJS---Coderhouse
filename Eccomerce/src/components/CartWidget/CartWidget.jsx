a2fbf2d8cb4ffd58ea360c9b8cf58de8599a38b2
Agustin R. Winer <r.w.agustin@gmail.com>
2023-12-16 15:46:14 -0300

    first commit

---
+++ Eccomerce/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,12 @@
+import cart from './Assets/cart.svg'
+
+const CartWidget = () => {
+    return(
+        <div>
+            <img src={cart} alt="" />
+            0
+        </div>
+    )
+}
+
+export default CartWidget
---
+++ clase1Reactjs/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NavBar from './components/NavBar/Navbar'
+
+function App() {
+  <div className='App'>
+    <NavBar/>
+  </div>
+}
+
+export default App
