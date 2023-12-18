0a470c92aa1e19240cf7e8ec25a88f9223d37a75
Agustin R. Winer <r.w.agustin@gmail.com>
2023-12-18 20:59:52 -0300

    Arreglo de componente

--- Eccomerce/src/components/ItemListContainer/ItemListContainer.jsx
+++ Eccomerce/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,9 @@
 const ItemListContainer = ( {greeting} ) =>{
-    <div>
-        <h1>{greeting}</h1>
-    </div>
+    return(
+        <div>
+            <h1>{greeting}</h1>
+        </div>
+    )
 }
 
 export default ItemListContainer
